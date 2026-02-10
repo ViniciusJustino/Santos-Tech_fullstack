42e9282ddece181a214dd8175110f124e5db8447
ViniciusJustino <36975057+ViniciusJustino@users.noreply.github.com>
2026-02-10 11:05:17 -0300

    Refactor program.js to use controllers and middleware

--- program.js
+++ program.js
@@ -1,17 +1,23 @@
 const rotas = require('express');
 const { usuario, admin} = require('./models/usuario');
+const diretorio = require('path');
+const controller_home = require('./controller/controller_home');
+const controller_login = require('./controller/controller_login');
 
 
 const rota = rotas();
+
+rota.use(rotas.urlencoded({extended: true}));
+rota.use(rotas.static(diretorio.join(__dirname + '/pages')));
+//rota.use(rotas.static(diretorio.join(__dirname + '/models')));
 
 const porta = '8080';
 
 const usuarios = [new usuario(1), new usuario(2), new usuario(3), new admin()];
 
-rota.get('/', (req, res) => {
-    res.send('<h1> Olá Mundo </h1>');
-});
+rota.get('/', controller_login.getLogin);
 
+rota.post('/login', controller_login.postUsuario);
 
 rota.listen(porta, () => {
 
