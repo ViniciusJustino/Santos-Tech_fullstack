f7648c6daf9eb23b69317a24773800602f11e118
ViniciusJustino <36975057+ViniciusJustino@users.noreply.github.com>
2026-02-11 18:06:03 -0300

    Refactor login controller to add home and user routes

--- controller/controller_login.js
+++ controller/controller_login.js
@@ -1,10 +1,10 @@
-const path = require('path')
+const diretorio = require('path');
+const models_usuarios = require('../models/usuario');
 
-module.exports.postUsuario = (req, res) => {
-    const {username, password , info_extra} = req.body;
-    console.log(username, password, info_extra);
+module.exports.getHome = (req, res) => {
+    res.sendFile(diretorio.resolve(__dirname + '/../pages/home.html'));
 };
 
-module.exports.getLogin = (req, res) =>{
-    res.sendFile(path.resolve(__dirname + '/../pages/login/login.html'));
-}
+module.exports.getUsuariosCadastrados = (req, res) => {
+    res.json(models_usuarios.getUsuarios());
+};
