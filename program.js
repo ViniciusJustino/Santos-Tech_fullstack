56ee0e60be451eba1fe5bbeec2fbb7c4e968bb1e
ViniciusJustino <36975057+ViniciusJustino@users.noreply.github.com>
2026-02-11 18:03:56 -0300

    Enhance security and update static file serving
    Added helmet middleware for security and updated static file serving method.

--- program.js
+++ program.js
@@ -1,23 +1,26 @@
 const rotas = require('express');
 const { usuario, admin} = require('./models/usuario');
 const diretorio = require('path');
+const helmet = require('helmet');
 const controller_home = require('./controller/controller_home');
 const controller_login = require('./controller/controller_login');
-
 
 const rota = rotas();
 
 rota.use(rotas.urlencoded({extended: true}));
-rota.use(rotas.static(diretorio.join(__dirname + '/pages')));
-//rota.use(rotas.static(diretorio.join(__dirname + '/models')));
+
+rota.use(helmet());
+
+rota.use(rotas.json());
+
+rota.use(rotas.static(diretorio.resolve(__dirname + '/pages')));
 
 const porta = '8080';
-
-const usuarios = [new usuario(1), new usuario(2), new usuario(3), new admin()];
 
 rota.get('/', controller_login.getLogin);
 
 rota.post('/login', controller_login.postUsuario);
+rota.get('/usuarios', controller_home.getUsuariosCadastrados);
 
 rota.listen(porta, () => {
 
