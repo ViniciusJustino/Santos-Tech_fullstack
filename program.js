623cf9e45260090fc287619d88158b8921aec589
ViniciusJustino <36975057+ViniciusJustino@users.noreply.github.com>
2026-02-25 18:08:46 -0300

    Add files via upload

--- program.js
+++ program.js
@@ -1,17 +1,30 @@
 const rotas = require('express');
-const { usuario, admin} = require('./models/usuario');
+const session = require('express-session');
+const rota = rotas();
+
+rota.use(rotas.json());
+
+rota.use(session({
+    secret: 'santos-tech',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000
+    }
+}));
+
+rota.use(rotas.urlencoded({extended: true}));
 const diretorio = require('path');
-const helmet = require('helmet');
+
+//onst helmet = require('helmet');
 const controller_home = require('./controller/controller_home');
 const controller_login = require('./controller/controller_login');
 
-const rota = rotas();
+const auth = require('./middleware/auth')
 
-rota.use(rotas.urlencoded({extended: true}));
 
-rota.use(helmet());
+//rota.use(helmet());
 
-rota.use(rotas.json());
 
 rota.use(rotas.static(diretorio.join(__dirname, '/pages')));
 
@@ -20,9 +33,8 @@ const porta = '8080';
 rota.get('/', controller_login.getLogin);
 
 rota.post('/login', controller_login.postUsuario);
-rota.get('/home', controller_home.getHome);
-
-rota.get('/usuarios', controller_home.getUsuariosCadastrados);
+rota.get('/home', auth ,controller_home.getHome);
+rota.get('/usuarios', auth , controller_home.getUsuariosCadastrados);
 
 rota.listen(porta, () => {
 
